085d5e20fd2776769f6366e0a75132f71751015b
jaKub <staniojakub@gmail.com>
2020-10-30 19:14:39 +0100

    Add initial client react application and Graphql client with fetching messages

---
+++ client/src/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Chat from '../components/Chat';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'shards-ui/dist/css/shards.min.css';
+import './app.css';
+
+const App = () => <Chat />;
+
+export default App;
